eb0e6429a9732fdd097f4d857f3b6bd048e91d31
seongeun308 <kse011010@gmail.com>
2025-12-23 12:15:01 +0900

    0002

---
+++ src/main/java/com/back/service/PostService.java
@@ -0,0 +1,22 @@
+package com.back.service;
+
+import com.back.entity.Member;
+import com.back.entity.Post;
+import com.back.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class PostService {
+
+    private final PostRepository postRepository;
+
+    public Post write(Member author, String title, String content) {
+        return postRepository.save(new Post(author, title, content));
+    }
+
+    public long count() {
+        return postRepository.count();
+    }
+}
