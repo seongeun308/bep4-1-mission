405dcd38e350c0ee121e0bb702391f66fc607195
seongeun308 <kse011010@gmail.com>
2025-12-24 11:24:58 +0900

    0011

--- src/main/java/com/back/global/initData/DataInit.java
+++ src/main/java/com/back/global/initData/DataInit.java
@@ -1,9 +1,9 @@
 package com.back.global.initData;
 
-import com.back.boundedContext.member.app.MemberFacade;
 import com.back.boundedContext.member.domain.Member;
-import com.back.boundedContext.post.app.PostFacade;
+import com.back.boundedContext.member.app.MemberFacade;
 import com.back.boundedContext.post.domain.Post;
+import com.back.boundedContext.post.app.PostFacade;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
@@ -57,12 +57,12 @@ public class DataInit {
         Member user2Member = memberFacade.findByUsername("user2").get();
         Member user3Member = memberFacade.findByUsername("user3").get();
 
-        Post post1 = postFacade.write(user1Member, "제목1", "내용1");
-        Post post2 = postFacade.write(user1Member, "제목2", "내용2");
-        Post post3 = postFacade.write(user1Member, "제목3", "내용3");
-        Post post4 = postFacade.write(user2Member, "제목4", "내용4");
-        Post post5 = postFacade.write(user2Member, "제목5", "내용5");
-        Post post6 = postFacade.write(user3Member, "제목6", "내용6");
+        Post post1 = postFacade.write(user1Member, "제목1", "내용1").getData();
+        Post post2 = postFacade.write(user1Member, "제목2", "내용2").getData();
+        Post post3 = postFacade.write(user1Member, "제목3", "내용3").getData();
+        Post post4 = postFacade.write(user2Member, "제목4", "내용4").getData();
+        Post post5 = postFacade.write(user2Member, "제목5", "내용5").getData();
+        Post post6 = postFacade.write(user3Member, "제목6", "내용6").getData();
     }
 
     @Transactional
