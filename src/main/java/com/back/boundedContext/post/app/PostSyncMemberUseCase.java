be81cb273d4a18ca853eeb19f7109b9f7052572c
seongeun308 <kse011010@gmail.com>
2025-12-26 15:28:43 +0900

    0030

--- src/main/java/com/back/boundedContext/post/app/PostSyncMemberUseCase.java
+++ src/main/java/com/back/boundedContext/post/app/PostSyncMemberUseCase.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class PostSyncMemberUseCase {
-    private PostMemberRepository postMemberRepository;
+    private final PostMemberRepository postMemberRepository;
 
     public PostMember syncMember(MemberDto member) {
         PostMember postMember = new PostMember(
