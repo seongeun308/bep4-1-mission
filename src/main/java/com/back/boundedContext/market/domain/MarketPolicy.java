4c4f618e884d0210d85677289991369ce42d7712
seongeun308 <kse011010@gmail.com>
2025-12-30 14:34:09 +0900

    0044

--- src/main/java/com/back/boundedContext/market/domain/MarketPolicy.java
+++ src/main/java/com/back/boundedContext/market/domain/MarketPolicy.java
@@ -7,6 +7,14 @@ import org.springframework.context.annotation.Configuration;
 public class MarketPolicy {
     public static double PRODUCT_PAYOUT_RATE;
 
+    public static long calculatePayoutFee(long salePrice, double payoutRate) {
+        return salePrice - calculateSalePriceWithoutFee(salePrice, payoutRate);
+    }
+
+    public static long calculateSalePriceWithoutFee(long salePrice, double payoutRate) {
+        return Math.round(salePrice * payoutRate / 100);
+    }
+
     @Value("${custom.market.product.payoutRate}")
     public void setProductPayoutRate(double rate) {
         PRODUCT_PAYOUT_RATE = rate;
